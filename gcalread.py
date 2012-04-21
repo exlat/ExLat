f873ba1aca999219448b4995d47dc4eca6b8a97f
swathy <swathysukumaran@gmail.com>
2012-04-21 21:21:47 +0530

    repeating monthly

--- gcalread.py
+++ gcalread.py
@@ -20,7 +20,7 @@
 #!/usr/bin/env python
 import sys
 import exchange as E
-
+import datetime
 def getListofDictionary(List, EventName):
   """Get a list of dictionaries. Each dictionary
      corresponds to a record of a single event
@@ -29,7 +29,7 @@ def getListofDictionary(List, EventName):
      Testing
   """
   EventCount = List.count(EventName)
-  EventsList = []
+ 
   End = 0
 
   # Iterate through all the records with SUMMARY = EventName
@@ -41,26 +41,119 @@ def getListofDictionary(List, EventName):
     Start = EventIndex - (List[EventIndex:0:-1].index('BEGIN'))
     End = List.index('END', EventIndex)
     KeyIndex = Start + 1
-
+    newlist=List[Start:End]
     for Entry in List[Start:End:2]:
       # FIXIT: If the event in the calender is not part of a series of events
       # the dates strings are different. That is if the event is planned as an
       # extra event
-      if Entry == 'DTSTART;TZID=Asia/Calcutta':
-        Dict['Start Date'] = List[KeyIndex][0:4]+'-'+List[KeyIndex][4:6]+'-'+List[KeyIndex][6:8]+' at '+List[KeyIndex][9:11]+':'+List[KeyIndex] [11:13]
-      if Entry == 'DTEND;TZID=Asia/Calcutta':
-        Dict['End Date'] = List[KeyIndex][0:4]+'-'+List[KeyIndex][4:6]+'-'+List[KeyIndex][6:8]+' at '+List[KeyIndex][9:11]+':'+List[KeyIndex][11:13]
-      if Entry == 'DESCRIPTION':
-        Dict['Topic'] = List[KeyIndex]
-      KeyIndex += 2
-    EventsList.append(Dict)
+      flag=0 
+      if Entry == 'RRULE':
+         index=newlist.index('RRULE') 
+	 EventsList = repeat(newlist,index)
+	 
+         flag=1
+         break
+    if flag == 0:
+    
+      
+         EventsList = dictionary(List,Start,End,KeyIndex)
+         
+         
 
   return EventsList
+
+def repeat(newlist,index):
+   """incrementing the month by 1 in start and end date"""
+   temp = newlist[index+1]
+   values=temp.split(';')
+   freq=values[0].split('=')
+   cnt=values[1].split('=')
+   count=[]
+   count.append(cnt[1])
+   c=map(int,count)
+   cnt=c[0]
+   start=newlist.index('BEGIN')
+   end=newlist.index('OPAQUE')
+   keyindex=start+1
+   arr = []
+   arr2 = []
+   i=0
+   
+   for entry in newlist[start:end:2]:
+      if entry == 'DTSTART;TZID=Asia/Calcutta':
+         s=datetime.datetime.strptime(newlist[keyindex],"%Y%m%dT%H%M%S")
+         arr.append(s.strftime('%Y'))
+         arr.append(s.strftime('%m'))
+         arr.append(s.strftime('%d'))
+         arr.append(s.strftime('%H'))
+         arr.append(s.strftime('%M'))
+         arr.append(s.strftime('%S'))
+
+      if entry == 'DTEND;TZID=Asia/Calcutta':
+         s=datetime.datetime.strptime(newlist[keyindex],"%Y%m%dT%H%M%S")
+         arr2.append(s.strftime('%Y'))
+         arr2.append(s.strftime('%m'))
+         arr2.append(s.strftime('%d'))
+         arr2.append(s.strftime('%H'))
+         arr2.append(s.strftime('%M'))
+         arr2.append(s.strftime('%S'))
+      
+      if entry == 'DESCRIPTION':
+         topic = newlist[keyindex]     
+
+      keyindex += 2
+
+   intarr=map(int,arr)
+   inityear=intarr[0]
+   initmonth=intarr[1]
+   initday=intarr[2]
+   inithour=intarr[3]
+   initminut=intarr[4]
+   intarr2=map(int,arr2)
+   finyear=intarr2[0]
+   finmonth=intarr2[1]
+   finday=intarr2[2]
+   finhour=intarr2[3]
+   finminut=intarr2[4]
+
+   if freq[1] == 'MONTHLY':
+  
+      while i < cnt :
+            Dict={} 
+            Dict['Start Date'] = '%i' %inityear+'-'+'%i' %initmonth+'-'+'%i' %initday+' at '+'%i' %inithour+':'+'%i' %initminut
+            Dict['End Date'] = '%i' %finyear+'-'+'%i' %finmonth+'-'+'%i' %finday+' at '+'%i' %finhour+':'+'%i' %finminut
+            Dict['Topic'] = topic
+            EventsList.append(Dict)       
+            initmonth += 1
+            finmonth += 1
+            i += 1
+            if initmonth > 12:
+               initmonth=1
+               inityear += 1
+            if finmonth > 12:
+               finmonth=1
+               finyear += 1
+   return EventsList
+def dictionary(List,Start,End,KeyIndex):
+      """dictionary when event is not repeating"""
+      Dict={}
+      for Entry in List[Start:End:2]:
+            if Entry == 'DTSTART':
+               s=datetime.datetime.strptime(List[KeyIndex],"%Y%m%dT%H%M%SZ")
+               Dict['Start Date'] =s.strftime('%Y-%m-%d-%H:%M')
+            if Entry == 'DTEND':
+               s=datetime.datetime.strptime(List[KeyIndex],"%Y%m%dT%H%M%SZ")
+               Dict['End Date'] =s.strftime('%Y-%m-%d-%H:%M')
+            if Entry == 'DESCRIPTION':
+               Dict['Topic'] = List[KeyIndex]
+            KeyIndex += 2
+      EventsList.append(Dict)      
+      return EventsList
 
 if len(sys.argv) != 3:
   print "Usage: ./gcalread.py <.ics file> <SUMMARY String>"
   sys.exit(1)
-
+EventsList = []
 ICSFile = sys.argv[1]
 SummaryString = sys.argv[2]
 F = open(ICSFile)
